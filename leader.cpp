26909b2bd0edcd0786b4ea350c6f9f50dfb49d51
Soumya Gupta <71893528+SoumyaGupta025@users.noreply.github.com>
2021-01-19 15:04:22 +0530

    leaders in array program

---
+++ leader.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std; 
+
+void Leaders(int arr[], int size) 
+
+{ 
+    
+	int i,j;
+    
+	for (i = 0; i < size; i++) 
+    
+	{ 
+        
+		for (j = i+1; j < size; j++) 
+        
+		{ 
+            
+			if (arr[i] <= arr[j]) 
+                
+				break; 
+        
+		}     
+        
+		if (j == size) 
+            
+			cout << arr[i] << " "; 
+  
+	} 
+
+} 
+  
+
+int main() 
+
+{ 
+      int arr[] = {16, 17, 4, 3, 5, 2}; 
+    
+	int n = sizeof(arr)/sizeof(arr[0]); 
+    
+	Leaders(arr, n); 
+    
+	return 0; 
+
+}
